0308ef9cfc3ae810095507d8ba169ae5d4bee2d8
shnymywk <shnymywk@gmail.com>
2026-05-14 14:08:56 +0900

    ✨ DBの値取得・取り出しおよび表示

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,22 +1,39 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import './Home.css'
+import { db } from '../firebase'
+import { collection, getDocs } from 'firebase/firestore'
 
 const Home = () => {
+    const [ postList, setPostList ] = useState([])
+
+    useEffect(() => {
+        const getPosts = async () => {
+            const data = await getDocs(collection(db, "posts"))
+            setPostList(data.docs.map((doc) => ({ ...doc.data(), id: doc.id})))
+        }
+        getPosts()
+    },[])
+
     return (
         <div className="homePage">
-            <div className="postContents">
-                <div className="postHeader">
-                    <h1>タイトル</h1>
-                </div>
-                
-                <div className="postTextContainer">
-                    sampleText_sampleText_sampleText_sampleText_sampleText_sampleText_sampleText_sampleText_sampleText_sampleText_sampleText_sampleText
-                </div>
-                <div className="nameAndDeleteButton">
-                    <h3>sampleName</h3>
-                    <button>削除</button>
-                </div>
-            </div>
+            {postList.map((post) => {
+                return (
+                    <div className="postContents" key ={post.id}>
+                        <div className="postHeader">
+                            <h1>{post.title}</h1>
+                        </div>
+                        
+                        <div className="postTextContainer">
+                            {post.postsText}
+                        </div>
+                        <div className="nameAndDeleteButton">
+                            <h3>@{post.author.username}</h3>
+                            <button>削除</button>
+                        </div>
+                    </div>
+                )
+            })}
+            
         </div>
     )
 };
