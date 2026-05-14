a9131b8e7736187d0d3576c1fc0099e38a5c04ac
shnymywk <shnymywk@gmail.com>
2026-05-14 13:05:47 +0900

    💄 HomePageの改修

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,8 +1,23 @@
 import React from 'react'
+import './Home.css'
 
 const Home = () => {
     return (
-        <div>Home</div>
+        <div className="homePage">
+            <div className="postContents">
+                <div className="postHeader">
+                    <h1>タイトル</h1>
+                </div>
+                
+                <div className="postTextContainer">
+                    sampleText_sampleText_sampleText_sampleText_sampleText_sampleText_sampleText_sampleText_sampleText_sampleText_sampleText_sampleText
+                </div>
+                <div className="nameAndDeleteButton">
+                    <h3>sampleName</h3>
+                    <button>削除</button>
+                </div>
+            </div>
+        </div>
     )
 };
 
